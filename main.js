6a8dd8ed01ba2c457f30f5f8ed6d3dc336c259d5
Emilianoo03 <emilianoo0309@gmail.com>
2021-07-29 22:36:30 -0300

    Solcion de errores

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ boton.addEventListener("click",function(){
     const nafta = document.getElementById('tipoNft').value;
     const Km = document.getElementById("CantKm").value;
     const KmL = document.getElementById("KmL").value;
-    if (isNaN(Km) || isNaN(KmL)) {
+    if (Km === "" || KmL === "" || nafta == 1) {
       document.getElementById("resultado").innerHTML =
       "<p id='resultado'>Ingresa los datos deseados</p>";
     } else {
